f499711c815a0601ba581b150d3407c3d870f240
Daniel Marjenburgh <dmarjenburgh@gmail.com>
2021-01-15 21:37:08 +0100

    Solve 2015 day 9

--- 2015/day9.cpp
+++ 2015/day9.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include <fstream>
-#include <map>
+#include <unordered_map>
 #include <regex>
 #include <array>
 #include <sstream>
 
-using distmap = std::map<std::string, int>;
+using distmap = std::unordered_map<std::string, int>;
 
-int distance(const std::array<std::string, 8> &cities, distmap &distances) {
+template<std::size_t SIZE>
+int distance(const std::array<std::string, SIZE> &cities, distmap &distances) {
     int total{};
-    for (const auto *p = cities.begin() + 1; p != cities.end(); ++p) {
-        total += distances[{*(p - 1) + *p}];
-    }
+    for (const auto *p = cities.begin() + 1; p != cities.end(); ++p)
+        total += distances[*(p - 1) + *p];
     return total;
 }
 
-int shortest_distance{0};
+int shortest_distance{std::numeric_limits<int>::max()};
+int longest_distance{0};
 
-void permute(std::array<std::string, 8> &cities, int k, distmap &m) {
-    if (k == 7) {
+template<std::size_t SIZE>
+void permute(std::array<std::string, SIZE> &cities, int k, distmap &m) {
+    if (k == SIZE - 1) {
         int d = distance(cities, m);
-        if (d < shortest_distance || shortest_distance == 0)
-            shortest_distance = d;
+        if (d < shortest_distance) shortest_distance = d;
+        if (d > longest_distance) longest_distance = d;
         return;
     }
-    for (int i = k; i < 8; ++i) {
+    for (int i = k; i < SIZE; ++i) {
         std::swap(cities[k], cities[i]);
         permute(cities, k + 1, m);
         std::swap(cities[k], cities[i]);
@@ -34,26 +36,24 @@ void permute(std::array<std::string, 8> &cities, int k, distmap &m) {
 int main() {
     std::ifstream input_file{"assets/2015/input9.txt"};
     std::string line{};
-    std::map<std::string, int> distances;
+    std::unordered_map<std::string, int> dists;
     std::array<std::string, 8> cities{};
-    std::vector<std::string> lines;
     int i{};
     while (std::getline(input_file, line)) {
-        lines.push_back(line);
-    }
-    input_file.close();
-    for (const std::string &l : lines) {
-        std::istringstream s{l};
+        std::istringstream s{line};
         std::string c1, to, c2, eq;
         int dist;
         s >> c1 >> to >> c2 >> eq >> dist;
-
-        distances[c1 + c2] = dist;
-        cities[i++] = c1;
+        std::string k1{};
+        dists[c1 + c2] = dist;
+        dists[c2 + c1] = dist;
+        if (c1 != cities[i - 1])
+            cities[i++] = c1;
+        if (i == 7)
+            cities[i] = c2;
     }
-    std::cout << i << cities[0] << std::endl;
 
-    permute(cities, 0, distances);
+    permute(cities, 0, dists);
     std::printf("Part 1: %d\n", shortest_distance);
-//    std::printf("Part 2: %d\n", enc_size - code_size);
+    std::printf("Part 2: %d\n", longest_distance);
 }
