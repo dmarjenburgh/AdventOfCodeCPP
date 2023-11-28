79f16c21a1cdb085a33889db8734edd8362ae126
Daniel Marjenburgh <daniel@bonsait.nl>
2023-11-28 17:03:02 +0100

    Solve 2022 day 5

---
+++ 2022/src/day5.cpp
@@ -0,0 +1,95 @@
+#include <vector>
+#include <regex>
+#include <ranges>
+#include <iostream>
+#include <numeric>
+#include "lib/IOUtils.hpp"
+
+using Stack = std::string;
+using Stacks = std::vector<Stack>;
+
+namespace ranges = std::ranges;
+
+struct Instruction {
+    int from;
+    int to;
+    int amount;
+};
+
+size_t numStacks(const std::string& line) {
+    const auto it{ranges::find_if(line.rbegin(), line.rend(), [](auto c) { return std::isdigit(c); })};
+    const char result{*it};
+    return result - '0';
+}
+
+
+std::tuple<Stacks, std::vector<Instruction>> parseInput() {
+    const auto lines{ReadAllLines("assets/input5.txt")};
+    auto div{ranges::find(lines, "")};
+    size_t num_stacks{numStacks(*(div-1))};
+
+    // Box characters are on indices: 1 + 4n
+    Stacks stacks{num_stacks};
+    for(auto it{lines.begin()};it!=div;++it) {
+        Stack s;
+        for(int idx{};idx<num_stacks;++idx) {
+            const char c{(*it)[1+4*idx]};
+            if (std::isalpha(c)) {
+                stacks[idx]+=c;
+            }
+        }
+    }
+    // Reverse stacks
+    ranges::for_each(stacks,ranges::reverse);
+
+    // Parse instructions
+    std::vector<Instruction> instructions;
+    const std::regex re{R"(move (\d+) from (\d+) to (\d+))"};
+    for(auto it{div+1}; it!=lines.end();++it) {
+        std::smatch sm;
+        std::regex_match(*it, sm, re);
+        instructions.push_back(Instruction{
+                .from = std::stoi(sm[2])-1,
+                .to = std::stoi(sm[3])-1,
+                .amount = std::stoi(sm[1])
+        });
+    }
+    return {stacks, instructions};
+}
+
+void makeMove(Stacks &state, const Instruction& instruction) {
+    for(int i{};i<instruction.amount;++i) {
+        const char last_char{state[instruction.from].back()};
+        state[instruction.to]+=last_char;
+        state[instruction.from].pop_back();
+    }
+}
+
+void makeMove2(Stacks &state, const Instruction& instruction) {
+    size_t from{state[instruction.from].size() - instruction.amount};
+    state[instruction.to].append(state[instruction.from].substr(from));
+    state[instruction.from].erase(from);
+}
+
+
+std::string topCrates(const Stacks &state) {
+    return std::accumulate(state.begin(), state.end(), std::string{}, [](auto acc, auto s) { return acc + s.back(); });
+}
+
+int main() {
+    auto [stacks, instructions]{parseInput()};
+    std::vector<Stack> stacks_copy{stacks};
+
+    for (const auto & item : instructions) {
+        makeMove(stacks, item);
+    }
+    std::cout << "Part 1: " << topCrates(stacks) << '\n';
+
+    stacks = std::move(stacks_copy);
+
+    for (const auto & item : instructions) {
+        makeMove2(stacks, item);
+    }
+
+    std::cout << "Part 2: " << topCrates(stacks) << '\n';
+}
